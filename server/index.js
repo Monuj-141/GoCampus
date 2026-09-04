0ab6805149427a1e064b23ef8859f00ad8c41364
Monuj Panging <monujpanging767@gmail.com>
2026-09-05 00:49:03 +0530

    Update API URL for production

--- server/index.js
+++ server/index.js
@@ -17,7 +17,12 @@ const PORT = process.env.PORT || 5000;
 // Enable CORS for frontend Vite client
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:3000","go-campus-green.vercel.app"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "http://localhost:3000",
+      "https://go-campus-green.vercel.app",
+    ],
     credentials: true,
   })
 );
